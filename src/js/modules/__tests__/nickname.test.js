713b34f47dab3ae1d10bc5b78ea698260b1340ee
Sergio chaplin <Sergiorabota133@gmail.com>
2022-12-29 21:39:02 +0300

    new task

--- src/js/modules/__tests__/nickname.test.js
+++ src/js/modules/__tests__/nickname.test.js
@@ -1,21 +1,33 @@
 import Validator from '../nickname';
 
-test('regular test true', () => {
-  const regular = new Validator();
-
-  const recevied = regular.validateUsername('name2w');
-
-  const expected = true;
-
-  expect(recevied).toEqual(expected);
+test('Валидное имя', () => {
+  expect(new Validator().validateUsername('m0nstercat')).toEqual('Ваше имя соответствует правилам!');
 });
 
-test('regular test false', () => {
-  const regular = new Validator();
+test('Невалидные символы в имени', () => {
+  expect(() => {
+    const name = new Validator();
+    return name.validateUsername('m0nsterкот)');
+  }).toThrow(new Error('В Вашем имени допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)!'));
+});
 
-  const recevied = regular.validateUsername('name2222w');
+test('Когда в имени больше трех цифр подряд', () => {
+  expect(() => {
+    const name = new Validator();
+    return name.validateUsername('m0004nstercat');
+  }).toThrow(new Error('В Вашем имени не может быть трёх цифр подряд!'));
+});
 
-  const expected = false;
+test('Невалидное значение в начале', () => {
+  expect(() => {
+    const name = new Validator();
+    return name.validateUsername('_m0nstercat');
+  }).toThrow(new Error('Ваше имя не может начинаться с цифр, тире или подчеркивания!'));
+});
 
-  expect(recevied).toEqual(expected);
+test('Невалидное значение в конце', () => {
+  expect(() => {
+    const name = new Validator();
+    return name.validateUsername('m0nstercat_');
+  }).toThrow(new Error('Ваше имя не может заканчиваться цифрами, тире или подчеркиванием!'));
 });
